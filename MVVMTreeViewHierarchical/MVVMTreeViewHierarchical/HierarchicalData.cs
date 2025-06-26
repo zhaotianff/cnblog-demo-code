5da1d9582210de5723eb037c9b79c1bd247f4265
jia.zhao <zhaotianff@163.com>
2025-06-26 09:17:32 +0800

    增加在WPF中使用MVVM实现TreeView的层级显示示例

---
+++ MVVMTreeViewHierarchical/MVVMTreeViewHierarchical/HierarchicalData.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMTreeViewHierarchical
+{
+    public class Level1 : ViewModelBase
+    {
+        private string level1Item = "";
+
+        public string Level1Item
+        {
+            get => level1Item;
+            set
+            {
+                level1Item = value;
+                RaiseChange("Level1Item");
+            }
+        }
+
+        private List<Level2> level1ChildList = new List<Level2>();
+
+        public List<Level2> Level1ChildList
+        {
+            get => level1ChildList;
+            set
+            {
+                level1ChildList = value;
+                RaiseChange("Level1ChildList");
+            }
+        }
+
+        public Level1(string item)
+        {
+            //构建测试数据
+            level1Item = item;
+            for(int i = 0;i<3;i++)
+            {
+                level1ChildList.Add(new Level2($"层级2项目@{i}"));
+            }
+        }
+    }
+
+    public class Level2 : ViewModelBase
+    {
+        private string level2Item = "";
+
+        public string Level2Item
+        {
+            get => level2Item;
+            set
+            {
+                level2Item = value;
+                RaiseChange("Level2Item");
+            }
+        }
+
+        private List<Level3> level2ChildList = new List<Level3>();
+
+        public List<Level3> Level2ChildList
+        {
+            get => level2ChildList;
+            set
+            {
+                level2ChildList = value;
+                RaiseChange("Level2ChildList");
+            }
+        }
+
+        public Level2(string item)
+        {
+            //构建测试数据
+            level2Item = item;
+            for (int i = 0; i < 3; i++)
+            {
+                level2ChildList.Add(new Level3($"层级3项目@{i}"));
+            }
+        }
+    }
+
+    public class Level3 : ViewModelBase
+    {
+        private string level3Item = "";
+
+        public string Level3Item
+        {
+            get => level3Item;
+            set
+            {
+                level3Item = value;
+                RaiseChange("Level3Item");
+            }
+        }
+
+        public Level3(string item)
+        {
+            //构建测试数据
+            level3Item = item;      
+        }
+    }
+}
---
+++ MVVMTreeViewHierarchical/MVVMTreeViewHierarchical/MainWindowViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMTreeViewHierarchical
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        private List<Level1> hierarchicalTestList = new List<Level1>();
+
+        public List<Level1> HierarchicalTestList
+        {
+            get => hierarchicalTestList;
+            set
+            {
+                hierarchicalTestList = value;
+                RaiseChange("HierarchicalTestList");
+            }
+        }
+
+        public MainWindowViewModel()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                hierarchicalTestList.Add(new Level1($"层级1项目@{i}"));
+            }
+        }
+    }
+}
