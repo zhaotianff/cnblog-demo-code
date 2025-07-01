da06692ecf3edcc30a2bec0a7e40c186d64410ec
jia.zhao <zhaotianff@163.com>
2025-07-01 09:23:01 +0800

    添加数据绑定调试示例代码

---
+++ DebugDataBinding/DebugDataBinding/MainWindowViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DebugDataBinding
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private string myProperty;
+
+        public string MyProperty 
+        { 
+            get => myProperty; 
+            set
+            {
+                myProperty = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("MyProperty"));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;  
+    }
+}
