85969870f0949013fdcd13f0a0a5deb8f9c19682
jia.zhao <zhaotianff@163.com>
2025-08-11 09:54:58 +0800

    增加独立VC++程序示例代码

---
+++ StandloneVC/StandloneMFC/StandloneMFCDlg.h
@@ -0,0 +1,34 @@
+﻿
+// StandloneMFCDlg.h: 头文件
+//
+
+#pragma once
+
+
+// CStandloneMFCDlg 对话框
+class CStandloneMFCDlg : public CDialogEx
+{
+// 构造
+public:
+	CStandloneMFCDlg(CWnd* pParent = nullptr);	// 标准构造函数
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_STANDLONEMFC_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
