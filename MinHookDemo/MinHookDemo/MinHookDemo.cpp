448cd09c7c6df0412282ae1e52c2dca3fe74722d
jia.zhao <zhaotianff@163.com>
2025-11-04 10:07:05 +0800

    增加MinHook示例

---
+++ MinHookDemo/MinHookDemo/MinHookDemo.cpp
@@ -0,0 +1,80 @@
+﻿// MinHookDemo.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include "minHook/MinHook.h"
+
+template <typename T>
+inline MH_STATUS MH_CreateHookEx(LPVOID pTarget, LPVOID pDetour, T** ppOriginal)
+{
+    return MH_CreateHook(pTarget, pDetour, reinterpret_cast<LPVOID*>(ppOriginal));
+}
+
+template <typename T>
+inline MH_STATUS MH_CreateHookApiEx(
+    LPCWSTR pszModule, LPCSTR pszProcName, LPVOID pDetour, T** ppOriginal)
+{
+    return MH_CreateHookApi(
+        pszModule, pszProcName, pDetour, reinterpret_cast<LPVOID*>(ppOriginal));
+}
+
+typedef int (WINAPI* MESSAGEBOXW)(HWND, LPCWSTR, LPCWSTR, UINT);
+
+// Pointer for calling original MessageBoxW.
+MESSAGEBOXW fpMessageBoxW = NULL;
+
+// Detour function which overrides MessageBoxW.
+int WINAPI DetourMessageBoxW(HWND hWnd, LPCWSTR lpText, LPCWSTR lpCaption, UINT uType)
+{
+    return fpMessageBoxW(hWnd, L"Hooked!", lpCaption, uType);
+}
+
+
+
+int main()
+{
+    // Initialize MinHook.
+    if (MH_Initialize() != MH_OK)
+    {
+        return 1;
+    }
+
+    // Create a hook for MessageBoxW, in disabled state.
+    if (MH_CreateHookEx(&MessageBoxW, &DetourMessageBoxW, &fpMessageBoxW) != MH_OK)
+    {
+        return 1;
+    }
+
+    // or you can use the new helper function like this.
+    //if (MH_CreateHookApiEx(
+    //    L"user32", "MessageBoxW", &DetourMessageBoxW, &fpMessageBoxW) != MH_OK)
+    //{
+    //    return 1;
+    //}
+
+    // Enable the hook for MessageBoxW.
+    if (MH_EnableHook(&MessageBoxW) != MH_OK)
+    {
+        return 1;
+    }
+
+    // Expected to tell "Hooked!".
+    MessageBoxW(NULL, L"Not hooked...", L"MinHook Sample", MB_OK);
+
+    // Disable the hook for MessageBoxW.
+    if (MH_DisableHook(&MessageBoxW) != MH_OK)
+    {
+        return 1;
+    }
+
+    // Expected to tell "Not hooked...".
+    MessageBoxW(NULL, L"Not hooked...", L"MinHook Sample", MB_OK);
+
+    // Uninitialize MinHook.
+    if (MH_Uninitialize() != MH_OK)
+    {
+        return 1;
+    }
+
+    return 0;
+}
