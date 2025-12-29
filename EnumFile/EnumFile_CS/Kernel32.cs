c873afcd7275af1cab33bf9c6ae764dc43423eda
jia.zhao <zhaotianff@163.com>
2025-12-29 09:32:49 +0800

    增加遍历文件示例

---
+++ EnumFile/EnumFile_CS/Kernel32.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumFile_CS
+{
+    public class Kernel32
+    {
+        [DllImport("Kernel32.dll")]
+        public static extern int GetLastError();
+
+        [DllImport("Kernel32.dll", CharSet = CharSet.Unicode)]
+        public static extern IntPtr FindFirstFile(string lpFileName, out WIN32_FIND_DATA lpFindFileData);
+
+        [DllImport("Kernel32.dll", CharSet = CharSet.Unicode)]
+        public static extern bool FindNextFile(IntPtr hFindFile, out WIN32_FIND_DATA lpFindFileData);
+
+        [DllImport("Kernel32.dll")]
+        public static extern bool FindClose(IntPtr hFindFile);
+
+        public static readonly IntPtr INVALID_HANDLE_VALUE = (IntPtr)(-1);
+        public static readonly uint FILE_ATTRIBUTE_DIRECTORY = 0x00000010;
+
+        public struct FILETIME
+        {
+            public uint dwLowDateTime;
+            public uint dwHighDateTime;
+        }
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+        public struct WIN32_FIND_DATA
+        {
+            /// <summary>
+            /// The file attributes of a file.
+            /// </summary>
+            /// <remarks>
+            /// Although the enum we bind to here exists in the .NET Framework
+            /// as System.IO.FileAttributes, it is not reliably present.
+            /// Portable profiles don't include it, for example. So we have to define our own.
+            /// </remarks>
+            public System.IO.FileAttributes dwFileAttributes;
+
+            /// <summary>
+            /// A FILETIME structure that specifies when a file or directory was created.
+            /// If the underlying file system does not support creation time, this member is zero.
+            /// </summary>
+            public FILETIME ftCreationTime;
+
+            /// <summary>
+            /// A FILETIME structure.
+            /// For a file, the structure specifies when the file was last read from, written to, or for executable files, run.
+            /// For a directory, the structure specifies when the directory is created.If the underlying file system does not support last access time, this member is zero.
+            /// On the FAT file system, the specified date for both files and directories is correct, but the time of day is always set to midnight.
+            /// </summary>
+            public FILETIME ftLastAccessTime;
+
+            /// <summary>
+            /// A FILETIME structure.
+            /// For a file, the structure specifies when the file was last written to, truncated, or overwritten, for example, when WriteFile or SetEndOfFile are used.The date and time are not updated when file attributes or security descriptors are changed.
+            /// For a directory, the structure specifies when the directory is created.If the underlying file system does not support last write time, this member is zero.
+            /// </summary>
+            public FILETIME ftLastWriteTime;
+
+            /// <summary>
+            /// The high-order DWORD value of the file size, in bytes.
+            /// This value is zero unless the file size is greater than MAXDWORD.
+            /// The size of the file is equal to(nFileSizeHigh* (MAXDWORD+1)) + nFileSizeLow.
+            /// </summary>
+            public uint nFileSizeHigh;
+
+            /// <summary>
+            /// The low-order DWORD value of the file size, in bytes.
+            /// </summary>
+            public uint nFileSizeLow;
+
+            /// <summary>
+            /// If the dwFileAttributes member includes the FILE_ATTRIBUTE_REPARSE_POINT attribute, this member specifies the reparse point tag.
+            /// Otherwise, this value is undefined and should not be used.
+            /// For more information see Reparse Point Tags.
+            /// </summary>
+            public uint dwReserved0;
+
+            /// <summary>
+            /// Reserved for future use.
+            /// </summary>
+            public uint dwReserved1;
+
+            /// <summary>
+            /// The name of the file.
+            /// </summary>
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 260)]
+            public string cFileName;
+
+            /// <summary>
+            /// An alternative name for the file.
+            /// This name is in the classic 8.3 file name format.
+            /// </summary>
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 14)]
+            public string cAlternateFileName;
+        }
+
+
+        public static void EnumerateSubDirectory(string dir)
+        {
+            // 搜索指定类型文件
+            string pszFileName;
+            string pTempSrc;
+            WIN32_FIND_DATA FileData = new WIN32_FIND_DATA();
+
+            // 构造搜索文件类型字符串, *.*表示搜索所有文件类型
+            pszFileName = $"{dir}\\*.*";
+
+            // 搜索第一个文件
+            IntPtr hFile = FindFirstFile(pszFileName, out FileData);
+            if (INVALID_HANDLE_VALUE != hFile)
+            {
+                do
+                {
+                    // 要过滤掉 当前目录"." 和 上一层目录"..", 否则会不断进入死循环遍历
+                    if ('.' == FileData.cFileName[0])
+                    {
+                        continue;
+                    }
+                    // 拼接文件路径	
+                    pTempSrc = $"{dir}\\{FileData.cFileName}";
+                    // 判断是否是目录还是文件
+                    if (FileData.dwFileAttributes == System.IO.FileAttributes.Directory)
+                    {
+                        // 目录, 则继续往下递归遍历文件
+                        EnumerateSubDirectory(pTempSrc);
+                    }
+                    else
+                    {
+                        // 文件
+                        Console.WriteLine(pTempSrc);
+                    }
+
+                    // 搜索下一个文件
+                } while (FindNextFile(hFile, out FileData));
+            }
+
+            // 关闭文件句柄
+            FindClose(hFile);
+        }
+    }
+}
