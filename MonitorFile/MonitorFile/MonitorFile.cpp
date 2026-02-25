a5fae9cde493e46d2ce1cddd7e11b2f98443abaf
jia.zhao <zhaotianff@163.com>
2026-02-25 10:25:01 +0800

    增加文件监控示例

---
+++ MonitorFile/MonitorFile/MonitorFile.cpp
@@ -0,0 +1,56 @@
+﻿// MonitorFile.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include<windows.h>
+
+int main()
+{
+	// 打开目录, 获取文件句柄
+	HANDLE hDirectory = ::CreateFile(L"D:\\PrintFiles\\", FILE_LIST_DIRECTORY,
+		FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING,
+		FILE_FLAG_BACKUP_SEMANTICS, NULL);
+	if (INVALID_HANDLE_VALUE == hDirectory)
+	{
+		return 0;
+	}
+
+
+	DWORD dwRet = 0;
+	DWORD dwBufferSize = 2048;
+
+	BYTE* pBuf = new BYTE[dwBufferSize];
+	FILE_NOTIFY_INFORMATION* pFileNotifyInfo = (FILE_NOTIFY_INFORMATION*)pBuf;
+	
+	BOOL bRet = ReadDirectoryChangesW(hDirectory,
+		pFileNotifyInfo, 
+		dwBufferSize,
+		TRUE,
+		FILE_NOTIFY_CHANGE_FILE_NAME|           //修改文件名
+		FILE_NOTIFY_CHANGE_ATTRIBUTES |			// 修改文件属性
+		FILE_NOTIFY_CHANGE_LAST_WRITE,			// 最后一次写入
+		&dwRet,
+		NULL, NULL);
+
+	if (FALSE == bRet)
+	{
+		DWORD dwError = GetLastError();
+		std::cout << "ReadDirectoryChangesW failed - " << dwError << std::endl;
+	}
+
+	std::wcout.imbue(std::locale("chs"));
+
+	//判断操作类型
+	switch (pFileNotifyInfo->Action)
+	{
+	case FILE_ACTION_ADDED:
+		 std::wcout << "Create file " << pFileNotifyInfo->FileName << std::endl;
+		break;
+	default:
+		break;
+	}
+
+	CloseHandle(hDirectory);
+	delete[] pBuf;
+}
+
---
+++ MonitorFile/MonitorFile_CS/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MonitorFile_CS
+{
+    public partial class Form1 : Form
+    {
+        FileSystemWatcher systemWatcher = new FileSystemWatcher();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+
+            if(folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                this.textBox1.Text = folderBrowserDialog.SelectedPath;
+            }
+        }
+
+
+
+        private void ShowMsg(string msg)
+        {
+            this.listBox1.Invoke(new Action(() => {
+                this.listBox1.Items.Add(msg);
+            }));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            systemWatcher.Path = this.textBox1.Text;
+
+            //设置监听的行为
+            //这里设置为文件名
+            systemWatcher.NotifyFilter = NotifyFilters.FileName;
+
+            //设置文件类型过滤
+            systemWatcher.Filter = "*.txt";
+
+            systemWatcher.Changed += (obj, args) => { ShowMsg($"文件更改{args.Name}"); };
+            //systemWatcher.Changed += SystemWatcher_Changed;  //上面是演示代码，为了方便。正常使用时应该创建函数，而不是直接使用匿名函数
+            systemWatcher.Created += (obj, args) => { ShowMsg($"文件创建{args.Name}"); };
+            systemWatcher.Deleted += (obj, args) => { ShowMsg($"文件删除{args.Name}"); };
+            systemWatcher.Renamed += (obj, args) => { ShowMsg($"文件重命名{args.Name}"); };
+
+            //开始监听
+            systemWatcher.EnableRaisingEvents = true;
+        }
+
+        private void SystemWatcher_Changed(object sender, FileSystemEventArgs e)
+        {
+            //处理逻辑
+        }
+    }
+}
